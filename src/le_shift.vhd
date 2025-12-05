78b7a45a55f9c693c7634bc1637f662eff19f0a1
NahlSyareza <availablearner@protonmail.com>
2025-12-05 19:00:44 +0700

    Added Encrypt - XOR Key and Sub Bytes. Working on Encrypt - Shifting

--- src/le_shift.vhd
+++ src/le_shift.vhd
@@ -4,9 +4,63 @@ USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY le_shift_entity IS
   PORT (
+    clk : IN STD_LOGIC;
+    reset : IN STD_LOGIC;
+    original_input : IN STD_LOGIC_VECTOR(127 DOWNTO 0);
+    altered_output : OUT STD_LOGIC_VECTOR(127 DOWNTO 0);
+    encrypt_or_decrypt : IN STD_LOGIC;
+    is_done : OUT STD_LOGIC
   );
 END le_shift_entity;
 
 ARCHITECTURE le_shift_architecture OF le_shift_entity IS
+  TYPE le_shift_states IS (IDLE, LOAD, SHIFT, DONE);
+  SIGNAL current_state, next_state : le_shift_states;
+
+  SIGNAL first_row : STD_LOGIC_VECTOR(31 DOWNTO 0);
+  SIGNAL second_row : STD_LOGIC_VECTOR (31 DOWNTO 0);
+  SIGNAL third_row : STD_LOGIC_VECTOR(31 DOWNTO 0);
+  SIGNAL fourth_row : STD_LOGIC_VECTOR(31 DOWNTO 0);
 BEGIN
+
+  logic_proc : PROCESS (reset, clk)
+  BEGIN
+    IF reset = '1' THEN
+      current_state <= IDLE;
+      is_done <= '0';
+    ELSIF rising_edge(clk) THEN
+      current_state <= next_state;
+    END IF;
+  END PROCESS;
+
+  shift_proc : PROCESS (current_state)
+  BEGIN
+    CASE (current_state) IS
+      WHEN IDLE =>
+        next_state <= LOAD;
+
+      WHEN LOAD =>
+        first_row <= original_input(127 DOWNTO 120) & original_input(95 DOWNTO 88) & original_input(63 DOWNTO 56) & original_input(31 DOWNTO 24);
+        second_row <= original_input(119 DOWNTO 112) & original_input(87 DOWNTO 80) & original_input(55 DOWNTO 48) & original_input(23 DOWNTO 16);
+        third_row <= original_input(111 DOWNTO 104) & original_input(79 DOWNTO 72) & original_input(47 DOWNTO 40) & original_input(15 DOWNTO 8);
+        fourth_row <= original_input(103 DOWNTO 96) & original_input(71 DOWNTO 64) & original_input(39 DOWNTO 32) & original_input(7 DOWNTO 0);
+        next_state <= SHIFT;
+
+      WHEN SHIFT <=
+        IF encrypt_or_decrypt = '0' THEN
+          second_row <= 
+        ELSE
+        END IF;
+        next_state <= DONE;
+
+      WHEN DONE =>
+        -- altered_output <=
+        is_done <= '1';
+
+      WHEN OTHERS =>
+        next_state <= IDLE;
+
+    END CASE;
+  END PROCESS;
+
 END le_shift_entity;
--- src/main.vhd
+++ src/main.vhd
@@ -7,18 +7,32 @@ ENTITY main_entity IS
     clk : IN STD_LOGIC;
     reset : IN STD_LOGIC;
     opcode : IN STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
-    inp : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-    outp : OUT STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0')
+    inp : IN STD_LOGIC_VECTOR(127 DOWNTO 0) := x"74F47ECF248D35E4BA56A0D6A29F5ACD";
+    key : IN STD_LOGIC_VECTOR(127 DOWNTO 0) := x"2B7E151628AED2A6ABF7158809CF4F3C";
+    outp : OUT STD_LOGIC_VECTOR(127 DOWNTO 0) := (OTHERS => '0')
   );
 END main_entity;
 
 ARCHITECTURE main_architecture OF main_entity IS
-  TYPE united_states IS (IDLE, START_ENCRYPT, START_DECRYPT, ENCRYPTING, DECRYPTING, FINISH_ENCRYPT, FINISH_DECRYPT, STORE_KEY);
+  TYPE united_states IS (IDLE, ENCRYPT_XOR, ENCRYPT_SUB, ENCRYPT_SHIFT, ENCRYPT_MIX, ENCRYPT_ROUND, DECRYPT_ROUND, DECRYPT_MIX, DECRYPT_SHIFT, DECRYPT_SUB, DECRYPT_XOR);
   SIGNAL current_state, next_state : united_states;
 
-  SIGNAL bffr : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
+  -- 0 Encrypt
+  -- 1 Decrypt
+  SIGNAL encrypt_or_decrypt : STD_LOGIC;
+
+  SIGNAL xor_outp : STD_LOGIC_VECTOR(127 DOWNTO 0) := (OTHERS => '0');
+
+  SIGNAL s_box_inp : STD_LOGIC_VECTOR(127 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL s_box_outp : STD_LOGIC_VECTOR(127 DOWNTO 0) := (OTHERS => '0');
 
 BEGIN
+
+  s_box : ENTITY work.s_box_entity
+    PORT MAP(
+      original_input => s_box_inp,
+      altered_output => s_box_outp
+    );
 
   logic_proc : PROCESS (reset, clk)
   BEGIN
@@ -29,25 +43,33 @@ BEGIN
     END IF;
   END PROCESS;
 
-  main_proc : PROCESS (inp, current_state)
+  main_proc : PROCESS (current_state, opcode)
   BEGIN
     next_state <= current_state;
 
     CASE (current_state) IS
       WHEN IDLE =>
         IF opcode = "0001" THEN
-          next_state <= START_ENCRYPT;
+          next_state <= ENCRYPT_XOR;
         ELSIF opcode = "0010" THEN
-          next_state <= START_DECRYPT;
-        ELSIF opcode = "0011" THEN
-          next_state <= STORE_KEY;
+          next_state <= DECRYPT_ROUND;
         ELSE
           next_state <= IDLE;
         END IF;
 
-      WHEN START_ENCRYPT =>
-        next_state <= ENCRYPTING;
-        bffr <= inp(0) & inp(3 DOWNTO 1);
+      WHEN ENCRYPT_XOR =>
+        encrypt_or_decrypt <= '0';
+        xor_outp <= inp XOR key;
+        next_state <= ENCRYPT_SUB;
+
+      WHEN ENCRYPT_SUB =>
+        s_box_inp <= xor_outp;
+        next_state <= ENCRYPT_SHIFT;
+
+        -- Start of Decryption
+      WHEN DECRYPT_ROUND =>
+        encrypt_or_decrypt <= 1;
+        next_state <= DECRYPT_MIX;
 
       WHEN OTHERS =>
         next_state <= IDLE;
